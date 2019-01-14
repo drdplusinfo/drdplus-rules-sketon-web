1e8eb218281e5c19934860e8edc56161721502c3
Jaroslav Týc <mail@jaroslavtyc.com>
2019-01-14 18:23:24 +0100

    Removed exhausting test of content

--- DrdPlus/Tests/RulesSkeletonWeb/RulesWebContentTest.php
+++ DrdPlus/Tests/RulesSkeletonWeb/RulesWebContentTest.php
@@ -16,32 +16,9 @@ class RulesWebContentTest extends AbstractContentTest
     /**
      * @test
      */
-    public function I_can_get_head(): void
-    {
-        $percents = $this->getTextsSimilarityPercents(
-            $this->getHtmlDocument()->head->innerHTML,
-            $this->getRulesWebContent()->getHtmlDocument()->head->innerHTML
-        );
-        self::assertGreaterThan(75, $percents);
-    }
-
-    private function getTextsSimilarityPercents(string $someText, string $anotherText): float
-    {
-        \similar_text(\str_replace("\n", '', \trim($someText)), \str_replace("\n", '', \trim($anotherText)), $percents);
-
-        return $percents;
-    }
-
-    /**
-     * @test
-     */
     public function I_can_get_body(): void
     {
-        $percents = $this->getTextsSimilarityPercents(
-            $this->getHtmlDocument()->body->innerHTML,
-            $this->getRulesWebContent()->getHtmlDocument()->body->innerHTML
-        );
-        self::assertGreaterThan(75, $percents);
+        self::assertNotEmpty($this->getRulesWebContent()->getHtmlDocument()->body->innerHTML);
     }
 
     /**
