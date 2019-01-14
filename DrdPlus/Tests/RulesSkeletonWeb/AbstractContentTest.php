8aae76896f411f068626443d7078ce1172b63f2d
Jaroslav Týc <mail@jaroslavtyc.com>
2019-01-14 18:15:15 +0100

    Tests optimized

--- DrdPlus/Tests/RulesSkeletonWeb/AbstractContentTest.php
+++ DrdPlus/Tests/RulesSkeletonWeb/AbstractContentTest.php
@@ -16,13 +16,6 @@ use Granam\WebContentBuilder\Web\WebFiles;
 
 abstract class AbstractContentTest extends TestWithMockery
 {
-    /** @var bool */
-    private static $skeletonChecked;
-    /** @var WebTestsConfiguration */
-    private $testsConfiguration;
-    /** @var Dirs */
-    private $dirs;
-
     protected function getHtmlDocument(): HtmlDocument
     {
         return $this->getRulesWebContent()->getHtmlDocument();
@@ -30,49 +23,61 @@ abstract class AbstractContentTest extends TestWithMockery
 
     protected function getRulesWebContent(): RulesWebContent
     {
-        $htmlHelper = new HtmlHelper($this->getDirs());
-        $head = new Head($htmlHelper, new CssFiles($this->getDirs(), true), new JsFiles($this->getDirs(), true));
-        $body = new Body(new WebFiles($this->getDirs()->getWebRoot()));
+        static $rulesWebContent;
+        if ($rulesWebContent === null) {
+            $htmlHelper = new HtmlHelper($this->getDirs());
+            $head = new Head($htmlHelper, new CssFiles($this->getDirs(), true), new JsFiles($this->getDirs(), true));
+            $body = new Body(new WebFiles($this->getDirs()->getWebRoot()));
 
-        return new RulesWebContent($htmlHelper, $head, $body);
+            $rulesWebContent = new RulesWebContent($htmlHelper, $head, $body);
+        }
+
+        return $rulesWebContent;
     }
 
     protected function isSkeletonChecked(string $skeletonProjectRoot = null): bool
     {
-        if (self::$skeletonChecked === null) {
+        static $skeletonChecked;
+        if ($skeletonChecked === null) {
             $projectRootRealPath = \realpath($this->getProjectRoot());
             self::assertNotEmpty($projectRootRealPath, 'Can not find out real path of document root ' . \var_export($this->getProjectRoot(), true));
             $skeletonRootRealPath = \realpath($skeletonProjectRoot ?? __DIR__ . '/../../../..');
             self::assertNotEmpty($skeletonRootRealPath, 'Can not find out real path of skeleton root ' . \var_export($skeletonRootRealPath, true));
 
-            self::$skeletonChecked = $projectRootRealPath === $skeletonRootRealPath;
+            $skeletonChecked = $projectRootRealPath === $skeletonRootRealPath;
         }
 
-        return self::$skeletonChecked;
+        return $skeletonChecked;
     }
 
     protected function getProjectRoot(): string
     {
-        self::assertDirectoryExists(\DRD_PLUS_PROJECT_ROOT, 'Project root has not been found');
+        static $projectRoot;
+        if ($projectRoot === null) {
+            self::assertDirectoryExists(\DRD_PLUS_PROJECT_ROOT, 'Project root has not been found');
+            $projectRoot = \DRD_PLUS_PROJECT_ROOT;
+        }
 
-        return \DRD_PLUS_PROJECT_ROOT;
+        return $projectRoot;
     }
 
     protected function getDirs(): Dirs
     {
-        if ($this->dirs === null) {
-            $this->dirs = new Dirs($this->getProjectRoot());
+        static $dirs;
+        if ($dirs === null) {
+            $dirs = new Dirs($this->getProjectRoot());
         }
 
-        return $this->dirs;
+        return $dirs;
     }
 
     protected function getTestsConfiguration(): WebTestsConfiguration
     {
-        if ($this->testsConfiguration === null) {
-            $this->testsConfiguration = WebTestsConfiguration::createFromYaml(\DRD_PLUS_TESTS_ROOT . '/tests_configuration.yml');
+        static $testsConfiguration;
+        if ($testsConfiguration === null) {
+            $testsConfiguration = WebTestsConfiguration::createFromYaml(\DRD_PLUS_TESTS_ROOT . '/tests_configuration.yml');
         }
 
-        return $this->testsConfiguration;
+        return $testsConfiguration;
     }
 }
